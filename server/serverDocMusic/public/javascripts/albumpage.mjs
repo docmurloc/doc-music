7766289185f466f9fbdbc194ef5d5086603bdb45
Pierre antoine <pierre.antoine@epitech.eu>
2020-08-15 19:10:24 +0200

    add form to delete or update album

--- server/serverDocMusic/public/javascripts/albumpage.mjs
+++ server/serverDocMusic/public/javascripts/albumpage.mjs
@@ -4,8 +4,16 @@ import {loadImageOption, loadPlaylistOption, loadAlbumOption} from './form.mjs'
 $( document ).ready( async function() {
     await loadImageOption("#albumArtwork");
     await loadPlaylistOption("#albumPlaylist");
+
     await loadAlbumOption("#albumToLink");
     await loadPlaylistOption("#PlaylistToLink");
+
+    await loadAlbumOption("#albumToChange");
+    await loadImageOption("#albumArtworkToChange");
+    await loadPlaylistOption("#albumPlaylistToChange");
+
+
+    await loadAlbumOption("#albumToDelete");
 
     console.log( "ready!" );
 });
