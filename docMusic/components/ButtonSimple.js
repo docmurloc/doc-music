3be818625e4c8a8fece6d30236d5c44cf10c3582
Pierre antoine <pierre.antoine@epitech.eu>
2020-09-08 03:28:39 +0200

    create simple button with style styleText and onPress as props

---
+++ docMusic/components/ButtonSimple.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {StyleSheet, Text, TouchableHighlight} from 'react-native';
+
+function ButtonSimple(props) {
+  
+    return (
+      <TouchableHighlight 
+      style={props.style}
+      onPress={() => {
+          props.onPress();
+      }}>
+        <Text style={props.styleText}>{props.text}</Text>
+      </TouchableHighlight>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+    },
+    box: {
+      width: '100%',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      paddingTop: 10,
+    },
+    content: {
+      margin: 30,
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      borderRadius: 20,
+    },
+    horizontalDisplay: {
+      width: '70 %',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    title: {
+      fontSize: 30,
+      textAlign: 'center',
+    },
+    textInput: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+    },
+  });
+  
+  const mapStateToProps = (state) => {
+    return state;
+  };
+  export default connect(mapStateToProps)(ButtonSimple);
