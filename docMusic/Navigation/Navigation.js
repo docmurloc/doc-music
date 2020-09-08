8dbf0dc0153b61137110f1f638057af361735f69
Pierre antoine <pierre.antoine@epitech.eu>
2020-09-08 03:27:29 +0200

    add first page to the naviation

--- docMusic/Navigation/Navigation.js
+++ docMusic/Navigation/Navigation.js
@@ -17,6 +17,7 @@ import PlaylistPage from '../components/PlaylistPage';
 import PlayerPage from '../components/PlayerPage';
 import ResearchPage from '../components/ResearchPage';
 import HistoryPage from '../components/HistoryPage';
+import FirstPage from '../components/Firstpage';
 
 const getProfilCache = async (props) => {
   try {
@@ -74,6 +75,11 @@ function MyStack(props) {
         }}>
         {props.profil.access_token == null ? (
           <>
+            <Stack.Screen
+              name="first"
+              component={FirstPage}
+              options={{headerShown: false}}
+            />
             <Stack.Screen
               name="Login"
               component={LoginPage}
