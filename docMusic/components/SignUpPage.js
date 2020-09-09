65ec7acf910401a88a9f88f8f20ed5354dc7acfa
Pierre antoine <pierre.antoine@epitech.eu>
2020-09-09 03:26:13 +0200

    change sign up page

--- docMusic/components/SignUpPage.js
+++ docMusic/components/SignUpPage.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 
 import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
-import BackgroundImage from './backgroundImage';
+import ButtonSimple from './ButtonSimple'
 
 import {registerUser} from '../APIserver/User';
 
@@ -15,46 +15,45 @@ function SignUpPage(props) {
 
   return (
     <View style={styles.container}>
-      <BackgroundImage />
-      <View style={styles.box}>
-        <View style={styles.content}>
-          <Text style={styles.title}>At one step of your dream</Text>
-          <Text style={styles.title}>{Info}</Text>
+      <View style={styles.empty}></View>
+        <View style={styles.box}>
+          <Text style={styles.textButton}>Sign up</Text>
+          <Text style={styles.textButton}>{Info}</Text>
           <TextInput
             style={styles.textInput}
             placeholder="Pseudo"
+            placeholderTextColor = 'rgba(169, 169, 169, 1)'
             onChangeText={(text) => setPseudo(text)}
             value={pseudo}
           />
           <TextInput
             style={styles.textInput}
             placeholder="Password"
+            placeholderTextColor = 'rgba(169, 169, 169, 1)'
             onChangeText={(text) => setPassword(text)}
             value={password}
           />
           <TextInput
             style={styles.textInput}
             placeholder="Confirm password"
+            placeholderTextColor = 'rgba(169, 169, 169, 1)'
             onChangeText={(text) => setConfirmPassword(text)}
             value={confirmpassword}
           />
-          <View style={styles.horizontalDisplay}>
-            <Button
-              title="REGISTER"
-              onPress={() => {
-                if (password === confirmpassword) {
-                  registerUser(props, pseudo, password, setInfo);
-                } else {
-                  setInfo('Password != confirm password');
-                }
-              }}
+      </View>
+      <View style={styles.content}>
+            <ButtonSimple
+            style={styles.Button}
+            styleText={styles.textButton}
+            text="Enter"
+            onPress={() => {
+              if (password === confirmpassword) {
+                registerUser(props, pseudo, password, setInfo);
+              } else {
+                setInfo('Password != confirm password');
+              }
+            }}
             />
-            <Button
-              title="SIGN IN"
-              onPress={() => props.navigation.navigate('Login')}
-            />
-          </View>
-        </View>
       </View>
     </View>
   );
@@ -65,21 +64,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgba(52, 52, 52, 1)',
   },
   box: {
-    width: '100%',
+    width: "70%",
+    margin: 10,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
     paddingTop: 10,
+    //backgroundColor: 'rgba(255, 52, 52, 1)',
   },
   content: {
     margin: 30,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'rgba(215, 215, 215, 0.9)',
-    borderRadius: 20,
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  //backgroundColor: 'rgba(52, 255, 52, 1)',
+
   },
   horizontalDisplay: {
     width: '70 %',
@@ -92,8 +94,30 @@ const styles = StyleSheet.create({
   },
   textInput: {
     height: 40,
-    borderColor: 'gray',
+    width: "100%",
+    textAlign: 'center',
+    backgroundColor: 'rgba(38, 38, 38, 1)',
+    color: 'rgba(169, 169, 169, 1)',
     borderWidth: 1,
+  },
+  empty: {
+    margin: 10,
+    flex: 0.5,
+    //alignItems: 'center',
+    //justifyContent: 'space-around',
+    //backgroundColor: 'rgba(52, 52, 255, 1)',
+
+  },
+  Button: {
+      //alignItems: 'center',
+      //justifyContent: 'center',
+      padding: 15,
+      borderRadius: 10,
+      //backgroundColor: 'rgba(100, 100, 100, 1)',  
+  },
+  textButton: {
+    fontSize: 20,
+    color: 'rgba(169, 169, 169, 1)',
   },
 });
 
